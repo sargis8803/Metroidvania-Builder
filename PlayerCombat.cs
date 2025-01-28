fc11546576f326bec9682707bc2c0871732d10f1
Iain <iain.valentine@wsu.edu>
2025-01-27 22:35:20 -0800

    Added placeholder script for player combat logic

---
+++ PlayerCombat.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerCombat : MonoBehavior
+{
+	// Placeholder variables for combat attributes.
+	public int attackDamage = 10;
+	public float attackRange = 1.5f;
+
+	// PLaceholder method for attacking.
+	public void Attack()
+	{
+
+		// TODO: Implement attack logic (detecting enemies, applying damage)
+		Debug.Log("Player attacked!");
+	}
+}
