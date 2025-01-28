96cab16b44d7838c956c54eef2061146d92f71ad
sargis <sargis.petrosyan@wsu.edu>
2025-01-27 21:24:45 -0800

    first ui set to push into repo

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
