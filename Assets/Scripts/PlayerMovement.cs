7f0387f182191ac7ecb3805bebda8937a077842a
Iain <iain.valentine@wsu.edu>
2025-02-07 15:31:07 -0800

    Added player movement and combat scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,47 +1,58 @@
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    private Rigidbody2D body;
-    private bool grounded;
+    // Public variables for movement speed and jump force, which is adjustable in Unity.
+    public float speed = 5f;
+    public float jumpForce = 7f;
 
-    private void Awake()
+    // Reference to the Rigidbody2D component for the physics interactions.
+    private Rigidbody2D rb;
+
+    // Boolean to check if the player is on the ground.
+    private bool isGrounded;
+
+    // Called once when the script starts.
+    void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        // Gets and stores the Rigidbody2D component attached to the player.
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    // Called once per frame.
+    void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        // Gets the horizontal movement input (-1 for left, 1 for right).
+        float moveInput = Input.GetAxis("Horizontal");
 
-        if (horizontalInput > 0.01)
-            transform.localScale = new Vector3(3, 3, 3);
-        else if (horizontalInput < -0.01)
-            transform.localScale = new Vector3(-3, 3, 3);
+        // Apply movement by modifying the player's velocity.
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
-            Jump();
+        // Checks if the jump button is pressed and the player is grounded.
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            // Apply an upward force to make the player jump.
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 
-    private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        grounded = false;
-    }
-
+    // Detects when the player collides with another object.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+        // If the player collides with an object tagged as "Ground", they are considered grounded.
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    // Detects when the player leaves contact with an object.
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (other.gameObject.CompareTag("Pickup"))
+        // If the player stops colliding with the ground, they are no longer grounded.
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            Destroy(other.gameObject);
+            isGrounded = false;
         }
     }
 }
