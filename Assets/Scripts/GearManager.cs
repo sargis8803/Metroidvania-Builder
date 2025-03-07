507165e7ed4a5870d09946147a70207601f6a61e
RomanEddy <159990314+RomanEddy@users.noreply.github.com>
2025-03-07 11:58:36 -0800

    Refines description box and pause/gear menu functionality
    Description boxs now only appear in selected slots that have an item in
    them.  Description boxes now show the name of the item in addition to
    the item's description.  Added MenuManager panel and accompanying script
    which fixes bug that allowed users to open both the pauseMenu and
    GearMenu where closing one would unpause the game.  Setup for Equipment
    tab which will begin development next commit.

--- Assets/Scripts/GearManager.cs
+++ Assets/Scripts/GearManager.cs
@@ -2,31 +2,22 @@ using UnityEngine;
 
 public class GearManager : MonoBehaviour
 {
-    public GameObject GearMenu;
-    private bool isOpen;
+    [SerializeField] MenuManager menuManager;
     public ItemSlot[] itemSlot;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I) &&  isOpen)
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            Time.timeScale = 1;
-            GearMenu.SetActive(false);
-            isOpen = false;
+            menuManager.ToggleGearMenu();
             DeselectAllSlots();
-        }
-        else if (Input.GetKeyDown(KeyCode.I) && !isOpen)
-        {
-            Time.timeScale = 0;
-            GearMenu.SetActive(true);
-            isOpen = true;
         }
     }
 
     public void AddItem(string itemName, Sprite itemSprite, string itemDescription)
     {
-        Debug.Log("itemName = " + itemName + "itemSprite = " + itemSprite);
+        //Debug.Log("itemName = " + itemName + "itemSprite = " + itemSprite);
         for (int i = 0; i < itemSlot.Length; i++)
         {
             if (itemSlot[i].isFull == false)
